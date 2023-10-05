bb01181103f49178e538b64d8a61f00a4724ca3d
Amrit Sundarka <amritsundarka@gmail.com>
2023-10-05 19:46:22 +0530

    Added Intital Files

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var aboutContent = document.querySelector('.about-content');
+var scrollPosition = window.scrollY;
+
+window.addEventListener('scroll', function() {
+  var newScrollPosition = window.scrollY;
+
+  if (newScrollPosition < scrollPosition) {
+    aboutContent.classList.add('slide-out');
+  } else {
+    aboutContent.classList.remove('slide-out');
+  }
+
+  scrollPosition = newScrollPosition;
+});
