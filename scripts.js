e764c206e10111af5adf0d5899545b7f1a626265
Amrit Sundarka <amritsundarka@gmail.com>
2023-10-05 19:52:13 +0530

    Added consoloe log

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ var scrollPosition = window.scrollY;
 
 window.addEventListener('scroll', function() {
   var newScrollPosition = window.scrollY;
+  console.log(newScrollPosition);
 
   if (newScrollPosition < scrollPosition) {
     aboutContent.classList.add('slide-out');
