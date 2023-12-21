64371c90caa7f812d58ed8282647a5e8d4d0d906
Amrit Sundarka <amritsundarka@gmail.com>
2023-12-21 14:12:24 +0530

    Chanegs to Web

--- scripts.js
+++ scripts.js
@@ -7,16 +7,19 @@ window.addEventListener('scroll', function() {
   var newScrollPosition = window.scrollY;
   console.log(newScrollPosition);
 
-  if (newScrollPosition < scrollPosition) {
-    aboutContent1.classList.add('slide-out');
-    aboutContent2.classList.add('slide-out');
-    aboutContent3.classList.add('slide-out');
-
+  if (newScrollPosition > 100) {
+    aboutContent1.classList.add('slide-in');
+  } else if (newScrollPosition > 200) {
+    aboutContent2.classList.add('slide-in');
+  } else if (newScrollPosition > 300) {
+    aboutContent3.classList.add('slide-in');
   } else {
     aboutContent1.classList.remove('slide-out');
     aboutContent2.classList.remove('slide-out');
     aboutContent3.classList.remove('slide-out');
   }
+
+  
 
   scrollPosition = newScrollPosition;
 });
